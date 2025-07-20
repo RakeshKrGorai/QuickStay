f4a82d16e47f437e9d51b9a5bc71c39be14c376e
RakeshKrGorai <rakesh.k.gorai@gmail.com>
2025-07-20 17:15:33 +0530

    Created Rooms Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, useLocation, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
 
 function App() {
   // checks whether the current path of the website has "owner" included within the url or not.
@@ -15,6 +16,7 @@ function App() {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
       <Footer />
